4788a436135b3f74e38972a69a538ebcb584c0d1
Alper Uyar <sezennani@gmail.com>
2023-06-05 13:39:46 +0200

    fix: feature login

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const port = 4000;
 app.use(express.urlencoded())
 
 
-app.get("/login.html", (req, res) => {
+app.get("https://alpashy.github.io/campus-rallye/", (req, res) => {
     res.sendFile(path.resolve("public/login.html"));
     
 });
