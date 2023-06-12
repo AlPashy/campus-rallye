d44319d6d98942f3216efa5168a40f9252d174dc
Alper Uyar <sezennani@gmail.com>
2023-06-12 13:15:19 +0200

    refactor: server works now

--- app.js
+++ app.js
@@ -1,37 +1,44 @@
 import express from 'express';
 import { fileURLToPath } from 'url';
-import { dirname } from 'path';
+import { dirname, join } from 'path';
 import fs from 'fs';
 
 const app = express();
+const port = 4001;
+
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
+const dataFilePath = join(__dirname, 'data.json');
 
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/public/felix.html');
+  res.sendFile(join(__dirname, 'public', 'login.html'));
 });
 
 app.post('/speichern', (req, res) => {
   const eingabe = req.body.eingabe;
 
-  const data = {
-    eingabe: eingabe
-  };
+  // Lese die vorhandenen Daten aus der JSON-Datei
+  let jsonData = [];
+  if (fs.existsSync(dataFilePath)) {
+    const data = fs.readFileSync(dataFilePath, 'utf8');
+    jsonData = JSON.parse(data);
+  }
 
-  const jsonData = JSON.stringify(data);
+  // Füge die neue Eingabe den Daten hinzu
+  if (!Array.isArray(jsonData)) {
+    jsonData = []; // Initialize as an empty array if not already
+  }
+  jsonData.push(eingabe);
 
-  fs.writeFile('data.json', jsonData, 'utf8', (err) => {
-    if (err) {
-      console.error(err);
-      res.send('Fehler beim Speichern der Daten!');
-    } else {
-      res.send('Daten wurden gespeichert!');
-    }
-  });
+  // Speichere die aktualisierten Daten in der JSON-Datei
+  fs.writeFileSync(dataFilePath, JSON.stringify(jsonData));
+
+  res.redirect('/');
 });
 
-app.listen(4000, () => {
-  console.log('Server gestartet auf http://localhost:4000');
+app.listen(port, () => {
+  console.log("App is listening on port", port);
 });
